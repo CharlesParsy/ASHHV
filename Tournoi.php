f1f09c7d3b950c1acd9c0f6747af670828ab1211
Parsy <parsycharlestom@gmail.com>
2024-03-27 21:44:12 +0100

    modifs

---
+++ Tournoi.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tournoi</title>
+    <link rel="shortcut icon" href="images/logo.jfif"/>
+    <link rel="stylesheet" href="style/style.css">
+    <script src="script/script.js" defer></script>
+</head>
+
+<body>
+<?php require_once("templates/menu.html"); ?>
+
+<br><br><br>
+<h2>Tournoi Internationnal René Pruvot</h2>
+
+<br>
+<p>La 4éme édition du Tournoi Internationnal René Pruvot aura lieu le samedi 18 et dimanche 19 mai 2024 au Complexe sportif Vauban de Valenciennes.</p>
+<br>
+<p>Ce tournoi handisportif 2024 doit être un moment unique de plaisir, de convivialité et d'échanges permettant de tisser des relations d'amitié entre citoyens européens et de valoriser chacun des participents.<br>
+Il s'inscrit dans un contexte Olympique inédit en France.<br>
+Nous souhaitons que tous, équipes, joueurs, encadrants, spectateurs, respectent les valeurs olympiques qui sont l'excellence, le respect et l'amitié ainsi que la philosophie du mouvement olympique.<br>
+Le mouvement olympique a pour but de contribuer à batir un monde pacifique et meilleur en éduquant la jeunesse par le moyen du sport pratiqué sans discrimination d'aucune sorte et dans l'esprit olympique qui exige la compréhension mutuelle, l'esprit d'amitié, la solidarité et le fair-play.</p>
+<h3>Equipes</h3>
+<p>10 équipes dont 4 françaises (ASHHV, Capelle La Grande, CAPSAA et Stade Rochelais), 3 équipes belges (BHBB, RBBC et Rolling Chair), 2 équipes italiennes (Seregno et Trento) et 1 équipe turc (Samsun) s'affronteront.</p>
+
+<h3>Concert</h3>
+<p>Pour l'occasion, le groupe Appetite (Tribute Guns N'Roses) viendra jouer le samedi 18 mai.</p>
+
+<br><br><br>
+
+<?php require_once("templates/footer.html"); ?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ASHHV</title>
+    <link rel="icon" href="images/logo.jfif">
+    <link rel="shortcut icon" href="images/logo.jfif"/>
+    <link rel="stylesheet" href="style/style.css">
+    <script src="script/script.js" defer></script>
+</head>
+
+<body>
+<?php require_once("templates/menu.html"); ?>
+
+<br><br><br><br><br>
+<img class="logo" src="images/logo.svg" alt="logo tournoi">
+<br><br><br><br><br>
+<div class="slogo"></div>
+
+<br><br><br>
+<h2>L'ASHHV fête ses 50 ans</h2>
+
+<p>C'est en 1974 que Jean Paul Miroux, ancien élève de René Pruvot (membre du Rotary), paraplégique, joueur de basket en équipe de France Handisport, champion de France et médaillé Paralympique, a souhaité voir se créer à Valenciennes une association.
+    Le Rotary Club de Valenciennes répond à son souhait et crée L'Association Sportive des Handicapés du Hainaut Valenciennes.</p>
+<br><br>
+
+<?php require_once("templates/footer.html"); ?>
+
+</body>
+</html>
