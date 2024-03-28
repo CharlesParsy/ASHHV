57bac4901d43500d22e0d81cba968f1229f7582f
Parsy <parsycharlestom@gmail.com>
2024-03-28 15:44:37 +0100

    test

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ASHHV</title>
-    <link rel="icon" href="images/logo.jfif">
-    <link rel="shortcut icon" href="images/logo.jfif"/>
+    <link rel="shortcut icon" type="image/x-icon" href="images/logo.ico.jpg">
+    <link rel="icon" type="image/jpg" href="images/logo.jpg">
     <link rel="stylesheet" href="style/style.css">
     <script src="script/script.js" defer></script>
 </head>
